e5cd39fd8dad3a93a814a8c0064fdf76dc4f1dbd
ryanward <ryanward@RYANWARDCE62>
2024-01-17 11:27:17 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Mission2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        RollDice rd = new RollDice();
+
+        int rollNum = 0;
+
+        System.Console.WriteLine("Welcome to the dice throwing simulator!");
+        System.Console.WriteLine("How many dice rolls would you like to simulate?");
+        string userInput = System.Console.ReadLine();
+
+        int.TryParse(userInput, out rollNum);
+
+        int[] results = rd.RollSim(rollNum);
+
+        System.Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+        System.Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        System.Console.WriteLine("Total number of rolls = " + rollNum);
+
+        for (int i = 0; i < results.Length; i++) 
+        {
+            int percentage = results[i] * 100 / rollNum;
+
+            System.Console.Write(i + 2 + ": ");
+            for (int iCount = 0; iCount < percentage; iCount++) 
+            {
+                System.Console.Write("*");
+            }
+            System.Console.WriteLine();
+        }
+
+        System.Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+    }
+}
---
+++ RollDice.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2
+{
+    public class RollDice
+    {
+        private Random random;
+
+        public RollDice()
+        {
+            random = new Random();
+        }
+        public int[] RollSim(int n)
+        {
+            int[] results = new int[11];
+
+            for (int i = 0; i < n; i++)
+            {
+                int dice1 = random.Next(1, 7);
+                int dice2 = random.Next(1, 7);
+
+                int sum = dice1 + dice2;
+
+                results[sum - 2]++;
+            }
+
+            return results;
+
+        }
+    }
+}
